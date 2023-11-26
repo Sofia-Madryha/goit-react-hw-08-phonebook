42c9795729adc852c167511204ca6f890b366126
Sofia-Madryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-11-26 00:04:33 +0000

    Deploying to gh-pages from @ Sofia-Madryha/goit-react-hw-08-phonebook@a74a900f99eace11794d32980c738152ac7fc51d 🚀

---
+++ static/js/791.e7436b98.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[791],{6791:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("div",{children:(0,r.jsx)("p",{children:"NotFoundPage"})})}}}]);
+//# sourceMappingURL=791.e7436b98.chunk.js.map
